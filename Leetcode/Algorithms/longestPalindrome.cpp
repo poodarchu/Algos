f63025f1189bc44c1289f4a041c588788d7517d7
Poodar Chu <poodarchu@gmail.com>
2017-04-18 18:35:44 +0800

    add comments.

--- Leetcode/Algorithms/longestPalindrome.cpp
+++ Leetcode/Algorithms/longestPalindrome.cpp
@@ -5,8 +5,8 @@ class Solution {
             L--;
             R++;
         }
-
-        return s.substr(L+1, R-L-1);
+        // 抵消 最后一个 L-- 和 R++ 的副作用
+        return s.substr(L+1, R-1-L);
     }
 public:
     string longestPalindrome(string s) {
@@ -18,9 +18,11 @@ public:
 
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); ++i) {
+            // p1 的长度为 奇数
             string p1 = expandAroundCenter(s, i, i);
             if (p1.length() > lp.length())
                 lp = p1;
+            // p1 的长度为 偶数
             string p2 = expandAroundCenter(s, i, i+1);
             if (p2.length() > lp.length())
                 lp = p2;
