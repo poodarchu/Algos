46597e2b39e8b3f20aa9a47017a693830ebd6ff3
Poodar Chu <poodarchu@gmail.com>
2017-04-19 00:34:44 +0800

    update.

--- Leetcode/Algorithms/zigzagConversion.cpp
+++ Leetcode/Algorithms/zigzagConversion.cpp
@@ -1,30 +1,36 @@
-class Solution {
+class ZigZagConversion {
 public:
+
     string convert(string s, int numRows) {
         if (s.length() <= numRows || numRows == 1)
             return s;
 
         string *output = new string[numRows];
-//        string* output = (string* )malloc(numRows* sizeof(string));
-//        memset(output,0,sizeof(output));
+
         int lengthOfGroup = 2*numRows - 2;
 
         int curr = 0;
         while ( curr < s.length() ) {
-            for (int row = 0; row < numRows; ++row) {
-                int currentRow = row;
-                while(currentRow < lengthOfGroup && (curr+currentRow) < s.length()) {
-                    output[row] += s[curr + currentRow];
-                    if ((2*(numRows-row) - 2) > 0)
-                        currentRow += 2*(numRows-row) - 2;
-                    else
-                        break;
+            for (int row = 0; row < numRows; row++) {
+
+                int currentIdx = row;
+
+                if (s[curr+currentIdx] == '\0')
+                    break;
+
+                output[row] += s[curr + currentIdx];
+
+                if ((2*(numRows-row) - 2) > 0) {
+                    currentIdx += 2 * (numRows - row) - 2;
+                    if (currentIdx < lengthOfGroup && (curr+currentIdx) < s.length())
+                        output[row] += s[curr + currentIdx];
                 }
+
             }
             curr += lengthOfGroup;
         }
 
-        string ret;
+        string ret = "";
         for (int i = 0; i < numRows; ++i)
             ret += output[i];
 
