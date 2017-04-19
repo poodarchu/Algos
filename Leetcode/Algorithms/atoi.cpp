2b108f74af28e69d4b22606feb1046166753e8df
Poodar Chu <poodarchu@gmail.com>
2017-04-19 12:41:22 +0800

    update.

--- Leetcode/Algorithms/atoi.cpp
+++ Leetcode/Algorithms/atoi.cpp
@@ -1,62 +1,71 @@
-class ATOI {
-    string strip(string s) {
-        int i = 0;
-        for (; i < s.length(); ++i)
-            if (s[i] != ' ')
-                break;
-        return s.substr(i, s.length());
-    }
+class Solution {
+//TODO: fix value overflow    
+  string strip(string s) {
+		int i = 0;
+		for (; i < s.length(); ++i)
+			if (s[i] != ' ')
+				break;
+		return s.substr(i, s.length());
+	}
 
-    bool containNonIntegerSymbol(string s) {
-        if (*s.begin() == '+' || *s.begin() == '-') {
-            for (string::iterator it = s.begin() + 1; it != s.end(); ++it)
-                if ((int) (*it) < int('0') || (int) *it > int('9'))
-                    return true;
-        } else
-            for (string::iterator it = s.begin(); it != s.end(); ++it)
-                if ((int)(*it) < int('0') || (int)*it > int('9'))
-                    return true;
-        return false;
-    }
+	string containNonIntegerSymbol(string s) {
+		if (*s.begin() == '+' || *s.begin() == '-') {
+			for (int i = 1; i < s.length(); ++i)
+				if ((int)s[i] >= int('0') && (int)s[i] <= int('9'))
+					continue;
+				else
+					return s.substr(0, i);
+		} else
+			for (int i = 0; i < s.length(); ++i)
+				if ((int)s[i] >= int('0') || (int)s[i] <= int('9'))
+					continue;
+				else
+					return s.substr(0, i);
+
+		return s;
+	}
 
 public:
-    int myAtoi(string s) {
-        stack<int> ret;
-        int retV = 0;
+	int myAtoi(string s) {
+		stack<int> ret;
+		int retV = 0;
 
-        if (s.empty())
-            return 0;
-        if (containNonIntegerSymbol(s))
-            return 0;
-        if (s == "+" || s == "-")
-            return 0;
-        else if (*s.begin() == '+') {
-            for (int i = 1; i < s.length(); ++i)
-                ret.push(((int) s[i] - (int)'0'));
-            int i = 0;
-            while(ret.size() > 0) {
-                retV += ret.top() * (int) pow(10, i++);
-                ret.pop();
-            }
-        } else if (*s.begin() == '-') {
-            for (int i = 1; i < s.length(); ++i)
-                ret.push(((int) s[i] - (int)'0'));
-            int i = 0;
-            while(ret.size() > 0) {
-                retV += ret.top() * (int) pow(10, i++);
-                ret.pop();
-            }
-            retV *= -1;
-        } else {
-            for (string::iterator it = s.begin(); it != s.end(); ++it)
-                ret.push((int)*it - (int)'0');
-            int i = 0;
-            while(ret.size() > 0) {
-                retV += ret.top() * pow(10, i++);
-                ret.pop();
-            }
-        }
+		s = strip(s);
+		s = containNonIntegerSymbol(s);
 
-        return retV;
-    }
+		if (s.empty())
+			return 0;
+	//    if (s = containNonIntegerSymbol(s))
+	//        return 0;
+		if (s == "+" || s == "-")
+			return 0;
+		else if (*s.begin() == '+') {
+			for (int i = 1; i < s.length(); ++i)
+				ret.push(((int) s[i] - (int)'0'));
+			int i = 0;
+			while(ret.size() > 0) {
+				retV += ret.top() * (int) pow(10, i++);
+				ret.pop();
+			}
+		} else if (*s.begin() == '-') {
+			for (int i = 1; i < s.length(); ++i)
+				ret.push(((int) s[i] - (int)'0'));
+			int i = 0;
+			while(ret.size() > 0) {
+				retV += ret.top() * (int) pow(10, i++);
+				ret.pop();
+			}
+			retV *= -1;
+		} else {
+			for (string::iterator it = s.begin(); it != s.end(); ++it)
+				ret.push((int)*it - (int)'0');
+			int i = 0;
+			while(ret.size() > 0) {
+				retV += ret.top() * pow(10, i++);
+				ret.pop();
+			}
+		}
+
+		return retV;
+	}
 };
